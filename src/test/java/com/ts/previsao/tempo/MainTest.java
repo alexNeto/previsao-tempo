a917995c8c233c62607b14b5e3f6d5f525f1d5a7
alexNeto <alex.neto3@hotmail.com>
2018-05-04 07:52:10 -0300

    adicionado mais testes

--- src/test/java/com/ts/previsao/tempo/MainTest.java
+++ src/test/java/com/ts/previsao/tempo/MainTest.java
@@ -1,7 +1,19 @@
 package com.ts.previsao.tempo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class MainTest {
 
-	
+	@Test
+	public void testa_se_define_port_padrao_caso_nao_definido() {
+		assertEquals(4567, Main.getHerokuAssignedPort());
+	}
+
+	@Test(timeout = 1000)
+	public void testa_se_main_nao_lanca_excecao() throws InterruptedException {
+		Main.main(null);
+	}
 
 }
